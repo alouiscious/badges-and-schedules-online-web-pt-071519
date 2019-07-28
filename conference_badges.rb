808a71aa9815da425e630e921be475acce658154
Renee Sims <info@reneesoul.com>
2019-07-28 17:55:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,44 @@
-# Write your code here.
+
+def badge_maker(first_name)
+
+ return "Hello, my name is #{first_name}."
+
+end
+
+
+def batch_badge_creator(names)
+  badge_messages = []
+
+   names.each do |names|
+     badge_messages << "Hello, my name is #{names}."
+   end
+ return badge_messages
+
+end
+
+def assign_rooms(attendees)
+ 
+  room_assignments = []
+  attendees.each do |name|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{room_assignments.size + 1}!"
+  end
+
+  return room_assignments
+  
+end
+
+
+def printer(attendees)
+    batch_badge_creator(attendees)
+    assign_rooms(attendees)
+
+  attendees {|name| puts batch_badge_creator(name).chomp}
+
+  
+
+end
+
+
+ 
+
+
